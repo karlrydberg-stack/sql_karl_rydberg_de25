9f3d05a4f372d9aaa338554d089defa3c9c309c9
Karl Rydberg <kalle.rydberg02@gmail.com>
2025-11-13 14:08:28 +0100

    Added some sql

---
+++ week1_query_bikedata/sql/ingest_bike.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE IF NOT EXISTS staging.orders AS (SELECT * FROM read_csv_auto('data\orders.csv'));
+
+CREATE TABLE IF NOT EXISTS staging.customers AS (SELECT * FROM read_csv_auto('data\customers.csv'));
